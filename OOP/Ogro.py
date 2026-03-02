670378f45410b3d6c9501f5c8ed9641963fec659
artaustin09 <a.r.t.austin09@gmail.com>
2026-03-02 15:29:08 -0800

    main

--- OOP/Ogro.py
+++ OOP/Ogro.py
@@ -1,8 +1,12 @@
 from Enemigo import *
-
+import random
 class Ogro(Enemigo):
     def __init__(self,puntos_energia=20,ataque=3):
         super().__init__(tipo_enemigo='Ogro', punto_energia=puntos_energia, ataque=ataque)
 
     def habla(self):
         print("Ogro aplastar todo!!!")  
+
+    def ataque_especial(sel):
+        print("ogrp ataque especial")
+        funciona_ataque_especial = random.random() < 0.20
